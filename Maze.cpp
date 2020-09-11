1f63ab4e0397142d8796dabf0ffae0d19d8b841d
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-09-11 19:39:13 +0200

    Update Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -1,12 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 #include "Maze.h"
 
 int Maze::createMaze(float height, float width)
 {
     GLFWwindow* window;
-
     /* Initialize the library */
     if(!glfwInit())
         return -1;
@@ -22,9 +22,25 @@ int Maze::createMaze(float height, float width)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+
     /* Loop until the user closes the window */
     while(!glfwWindowShouldClose(window))
     {
+        int DOWN = glfwGetKey(window, GLFW_KEY_DOWN);
+        int UP   = glfwGetKey(window, GLFW_KEY_UP);
+        int LEFT = glfwGetKey(window, GLFW_KEY_LEFT);
+        int RIHT = glfwGetKey(window, GLFW_KEY_RIGHT);
+
+        if(DOWN == GLFW_PRESS)
+        {
+            std::cout << "wtf" << std::endl;  
+        }
+
+        if(UP == GLFW_PRESS)
+        {
+            std::cout << "omg" << std::endl;
+        }
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -49,11 +65,13 @@ void Maze::movePlayer()
 
 }
 
+
+
 int main(void)
 {
     Maze o_maze;
     o_maze.createMaze(1280, 720);
-
+    
     //unsigned int buffer;
     //glGenBuffers(1, &buffer);
     //glBindBuffer(GL_ARRAY_BUFFER, buffer);
