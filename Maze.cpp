160c27752ca3db6036fbf1b9644cbf24e0fd21bd
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-09-15 09:59:04 +0200

    Update Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -1,111 +1,82 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GL/glut.h>
 
 #include <iostream>
-#include "Maze.h"
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+float posX, posY;
+
+float pointVertex2[] = {
+    1280 * 0.75, 360
+};
+
+void renderScene(void)
 {
-    switch(key)
-    {
-    default:
-        if(action == GLFW_PRESS)
-            std::cout << "spatne tlacitko" << std::endl;
-        break;
+	glClear(GL_COLOR_BUFFER_BIT);
 
-    case GLFW_KEY_UP:
-        if(action == GLFW_PRESS)
-            std::cout << "Nahoru" << std::endl;
-        if(action == GLFW_REPEAT)
-            std::cout << "Nahoru - opak" << std::endl;
-        break;
+	glEnable(GL_POINT_SMOOTH);
+	glBegin(GL_ARRAY_BUFFER);
+	glVertexPointer(2, GL_FLOAT, 0, pointVertex2);
+	glPointSize(50);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glEnd();
+	glDisable(GL_POINT_SMOOTH);
 
-    case GLFW_KEY_DOWN:
-        if(action == GLFW_PRESS)
-            std::cout << "Dolu" << std::endl;
-        if(action == GLFW_REPEAT)
-            std::cout << "Dolu - opak" << std::endl;
-        break;
-
-    case GLFW_KEY_LEFT:
-        if(action == GLFW_PRESS)
-            std::cout << "Doleva" << std::endl;
-        if(action == GLFW_REPEAT)
-            std::cout << "Doleva - opak" << std::endl;
-        break;
-
-    case GLFW_KEY_RIGHT:
-        if(action == GLFW_PRESS)
-            std::cout << "Doprava" << std::endl;
-        if(action == GLFW_REPEAT)
-            std::cout << "Doprava - opak" << std::endl;
-        break;
-    }
+	glutSwapBuffers();
 }
 
-int Maze::createMaze(float height, float width)
+void bad_key_callback(unsigned char key, int x, int y)
 {
-    GLFWwindow* window;
-    /* Initialize the library */
-    if(!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow((int)height, (int)width, "Maze Game", NULL, NULL);
-    if(!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    float pointVertex2[] = {
-        1280 * 0.75, 360
-    };
-
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
-
-    /* Loop until the user closes the window */
-    while(!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        
-        glEnable(GL_POINT_SMOOTH);
-        glBegin(GL_ARRAY_BUFFER);
-        glVertexPointer(2, GL_FLOAT, 0, pointVertex2);
-        glPointSize(50);
-        glDrawArrays(GL_POINTS, 0, 1);
-        glEnd();
-        glDisable(GL_POINT_SMOOTH);
-        
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+	if(key == 27)
+		exit(0);
+	else
+		std::cout << "spatne tlacitko" << std::endl;
 }
 
-void Maze::resetMaze()
+void key_callback(int key, int x, int y)
 {
+	switch(key)
+	{
+	default:
+		std::cout << "spatne tlacitko" << std::endl;
+		break;
 
+	case GLUT_KEY_UP:
+		std::cout << "Nahoru" << std::endl;
+		posY += 0.05f;
+		break;
+
+	case GLUT_KEY_DOWN:
+		std::cout << "Dolu" << std::endl;
+		posY -= 0.05f;
+		break;
+
+	case GLUT_KEY_LEFT:
+		std::cout << "Doleva" << std::endl;
+		posX -= 0.05f;
+		break;
+
+	case GLUT_KEY_RIGHT:
+		std::cout << "Doprava" << std::endl;
+		posX += 0.05f;
+		break;
+	}
 }
 
-void Maze::movePlayer()
+int main(int argc, char** argv)
 {
-    
-}
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(640, 360);
+	glutCreateWindow("Maze Game");
 
-int main(void)
-{
-    Maze game;
-    game.createMaze(1280, 720);
-    
+	glutDisplayFunc(renderScene);
+
+	glutKeyboardFunc(bad_key_callback);
+	glutSpecialFunc(key_callback);
+
+	glutMainLoop();
+
+	return 0;
 }
