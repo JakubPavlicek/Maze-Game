0b64becfd2929502ab5b2e932ba702db7434c886
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-09-22 14:30:26 +0200

    Update macos_version.cpp

--- macos_version.cpp
+++ macos_version.cpp
@@ -2,8 +2,11 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cstdio>
 
 float posX = 0, posY = 0;
+
+int WIDTH = 640, HEIGHT = 480;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -78,57 +81,68 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 int main(void)
 {
     GLFWwindow* window;
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-        /* Initialize the library */
-        if (!glfwInit())
-            return -1;
-
-        /* Create a windowed mode window and its OpenGL context */
-        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-        if (!window)
-        {
-            glfwTerminate();
-            return -1;
-        }
-
-        /* Make the window's context current */
-        glfwMakeContextCurrent(window);
-        
-        // get version info
-        const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-        const GLubyte* version = glGetString(GL_VERSION); // version as a string
-        printf("Renderer: %s\n", renderer);
-        printf("OpenGL version supported %s\n", version);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-        glfwSetKeyCallback(window, key_callback);
-    
-        /* Loop until the user closes the window */
-        while (!glfwWindowShouldClose(window))
-        {
-            /* Render here */
-            glClear(GL_COLOR_BUFFER_BIT);
-            glClearColor(0.3f, 0.3f, 0.3f, 1);
-            glColor3f(0, 0.8, 0);
-            glBegin(GL_QUADS);
-            glVertex2f(-0.03f + posX, 0.03f + posY);
-            glVertex2f(0.03f + posX, 0.03f + posY);
-            glVertex2f(0.03f + posX, -0.03f + posY);
-            glVertex2f(-0.03f + posX, -0.03f + posY);
-            glEnd();
-            
-            /* Swap front and back buffers */
-            glfwSwapBuffers(window);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-            /* Poll for and process events */
-            glfwPollEvents();
-        }
-
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Maze Game", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+    
+    glfwSetKeyCallback(window, key_callback);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.3f, 0.3f, 0.3f, 1);
+        
+        // player's GL_QUADS
+        glColor3f(0, 0.8, 0);
+        glBegin(GL_QUADS);
+        glVertex2f(-1.0f + posX, -0.9f + posY);
+        glVertex2f(-0.9f + posX, -0.9f + posY);
+        glVertex2f(-0.9f + posX, -1.0f + posY);
+        glVertex2f(-1.0f + posX, -1.0f + posY);
+        glEnd();
+        
+        // end's GL_QUADS
+        glColor3f(1, 0, 0);
+        glBegin(GL_QUADS);
+        glVertex2f(0.9f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+        glVertex2f(1.0f, 0.9f);
+        glVertex2f(0.9f, 0.9f);
+        glEnd();
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     
     return 0;
 }
