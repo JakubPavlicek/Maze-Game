309995c8b66f4bb2688b68a5d667b5d304d15848
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-09-24 08:55:15 +0200

    Update macos_version.cpp

--- macos_version.cpp
+++ macos_version.cpp
@@ -25,11 +25,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         {
             case GLFW_PRESS:
                 std::cout << "Nahoru" << std::endl;
-                posY += 0.03f;
+                posY += 0.05f;
                 break;
             case GLFW_REPEAT:
                 std::cout << "Nahoru - opak" << std::endl;
-                posY += 0.03f;
+                posY += 0.05f;
                 break;
         }
         break;
@@ -39,11 +39,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         {
             case GLFW_PRESS:
                 std::cout << "Dolu" << std::endl;
-                posY -= 0.03f;
+                posY -= 0.05f;
                 break;
             case GLFW_REPEAT:
                 std::cout << "Dolu - opak" << std::endl;
-                posY -= 0.03f;
+                posY -= 0.05f;
                 break;
         }
         break;
@@ -53,11 +53,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         {
             case GLFW_PRESS:
                 std::cout << "Doleva" << std::endl;
-                posX -= 0.03f;
+                posX -= 0.05f;
                 break;
             case GLFW_REPEAT:
                 std::cout << "Doleva - opak" << std::endl;
-                posX -=  0.03f;
+                posX -=  0.05f;
                 break;
         }
         break;
@@ -67,11 +67,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         {
             case GLFW_PRESS:
                 std::cout << "Doprava" << std::endl;
-                posX += 0.03f;
+                posX += 0.05f;
                 break;
             case GLFW_REPEAT:
                 std::cout << "Doprava - opak" << std::endl;
-                posX += 0.03f;
+                posX += 0.05f;
                 break;
         }
     break;
@@ -135,6 +135,37 @@ int main(void)
         glVertex2f(0.9f, 0.9f);
         glEnd();
         
+        // walls
+        glColor3f(1,1,1);
+//        glLineWidth(5);
+//        glBegin(GL_LINES);
+//        glVertex2f(-0.2, 0);
+//        glVertex2f(-0.2, 1);
+//        glEnd();
+//
+//        glLineWidth(5);
+//        glBegin(GL_LINES);
+//        glVertex2f(-0.6, 0);
+//        glVertex2f(-0.6, 1);
+//        glEnd();
+//
+//        glLineWidth(5);
+//        glBegin(GL_LINES);
+//        glVertex2f(0, -0.5);
+//        glVertex2f(1, -0.5);
+//        glEnd();
+        
+        glLineWidth(5);
+        glBegin(GL_LINES);
+        glVertex2f(-1, -0.3);
+        glVertex2f(0, -0.3);
+        glEnd();
+        
+        // Lose
+        if(posX >= -0.1f && posX <= 1.0f && posY >= 0.65f && posY <= 0.7f)
+            std::cout << "PROHRAL SI" << std::endl;
+        
+        // Win
         if(posX >= 1.8 && posY >= 1.8)
             exit(0);
         
