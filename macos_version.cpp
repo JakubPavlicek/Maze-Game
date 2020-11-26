19202002200aecf8e4feef9dd107e303f7f991fe
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-11-26 09:25:05 +0100

    Update macos_version.cpp

--- macos_version.cpp
+++ macos_version.cpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <cstdio>
 #include <utility>
+#include <map>
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
-#include FT_GLYPH_H
+//#include FT_GLYPH_H
+
+#include "glm.hpp"
 
 static float posX = 0, posY = 0;
 
@@ -99,27 +102,16 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
-// FREETYPE
-
-// END OF FREETYPE
+struct Character
+{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2 Size;        // Size of glyph
+    glm::ivec2 Bearing;     // Offset from baseline to left/top glyph
+    unsigned int Advance;   // Offset to advance to next glyph
+};
 
 int main()
 {
-    FT_Library  library;
-
-    FT_Error error = FT_Init_FreeType( &library );
-    if ( error )
-    {
-        std::cout << "Error occured during FT initialisation" << std::endl;
-    }
-    
-    FT_Face face;
-    if (FT_New_Face(library, "/System/Library/Fonts/Helvetica.ttc", 0, &face))
-    {
-        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-        return -1;
-    }
-    
     GLFWwindow* window;
     
     /* Initialize the library */
@@ -144,6 +136,79 @@ int main()
     
     glewExperimental = GL_TRUE;
     glewInit();
+
+// FREETYPE
+    FT_Library ft;
+
+    FT_Error error = FT_Init_FreeType( &ft );
+    if ( error )
+    {
+        std::cout << "Error occured during FT initialisation" << std::endl;
+    }
+    
+    FT_Face face;
+    if (FT_New_Face(ft, "/System/Library/Fonts/Helvetica.ttc", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+    
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    
+    if(FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+        return -1;
+    }
+    
+    std::map<char, Character> Characters;
+    
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+    
+    for(unsigned int i = 0; i < 128; i++)
+    {
+        // load character glyph
+        if(FT_Load_Char(face, i, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        
+        // generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            (unsigned int)face->glyph->advance.x
+        };
+        Characters.insert(std::pair<char, Character>(i, character));
+    }
+    
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+// END OF FREETYPE
     
     // get version info
     const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
@@ -235,4 +300,3 @@ int main()
     
     return 0;
 }
-
